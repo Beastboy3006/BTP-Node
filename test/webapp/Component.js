bddccf8ce8f0fe100a22bcb181efe3644f2d0b85
Karthik Mahalingam <k.mahalingam@sierradigitalinc.com>
2022-11-26 12:15:27 +0000

    november commit

---
+++ test/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("app.test.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
